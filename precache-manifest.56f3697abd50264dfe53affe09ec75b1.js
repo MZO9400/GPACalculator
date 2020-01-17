c74495aee74c158ad33db1872691d905487b857f
MZO9400 <hamza.hameed00@yahoo.com>
2020-01-17 23:23:20 +0500

    Updates

---
+++ precache-manifest.56f3697abd50264dfe53affe09ec75b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70f81979b8fb9da71918c976ead55ca",
+    "url": "/GPACalculator/index.html"
+  },
+  {
+    "revision": "9d8ced742c1f238dd176",
+    "url": "/GPACalculator/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d53531be931556e5784e",
+    "url": "/GPACalculator/static/css/main.35940875.chunk.css"
+  },
+  {
+    "revision": "9d8ced742c1f238dd176",
+    "url": "/GPACalculator/static/js/2.8d33e539.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/GPACalculator/static/js/2.8d33e539.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d53531be931556e5784e",
+    "url": "/GPACalculator/static/js/main.595f7285.chunk.js"
+  },
+  {
+    "revision": "5800556e62dd36be1538",
+    "url": "/GPACalculator/static/js/runtime-main.c5cb091f.js"
+  }
+]);
