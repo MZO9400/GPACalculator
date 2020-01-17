c1c576400deab8fdc7275c8ebb4df82435f91cd2
MZO9400 <hamza.hameed00@yahoo.com>
2020-01-17 18:31:38 +0500

    Fix per semester GPA and CGPA calculation

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Button from "react-bootstrap/Button";
 class GPACalculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { Subjects: [] };
+    this.state = { Semesters: [[{ Name: "", Grade: 0, CreditHours: 0 }]] };
     this.insertNewSubject = this.insertNewSubject.bind(this);
     this.handleGradeChange = this.handleGradeChange.bind(this);
     this.handleNameChange = this.handleNameChange.bind(this);
@@ -21,38 +21,52 @@ class GPACalculator extends React.Component {
     if (isNaN(index)) {
       index = 0;
     }
+    let col = parseInt(e.target.getAttribute("col"));
+    if (isNaN(col)) {
+      col = 0;
+    }
     let value = e.target.value;
-    this.setState(state => (state.Subjects[index].CreditHours = value));
+    this.setState(state => (state.Semesters[col][index].CreditHours = value));
   }
   handleGradeChange(e) {
     let index = parseInt(e.target.id);
     if (isNaN(index)) {
       index = 0;
     }
+    let col = parseInt(e.target.getAttribute("col"));
+    if (isNaN(col)) {
+      col = 0;
+    }
     let value = e.target.value;
-    this.setState(state => (state.Subjects[index].Grade = value));
+    this.setState(state => (state.Semesters[col][index].Grade = value));
   }
   handleNameChange(e) {
     let index = parseInt(e.target.id);
     if (isNaN(index)) {
       index = 0;
     }
+    let col = parseInt(e.target.getAttribute("col"));
+    if (isNaN(col)) {
+      col = 0;
+    }
     let value = e.target.value;
-    this.setState(state => (state.Subjects[index].Name = value));
+    this.setState(state => (state.Semesters[col][index].Name = value));
   }
-  returnSubjectJSON(value) {
+  returnSubjectJSON(value, col) {
     return (
       <Form key={value}>
         <Form.Control
           onChange={this.handleNameChange}
           placeholder="Enter Subject Name"
           id={value}
+          col={col}
           value={this.state.value}
         ></Form.Control>
         <Form.Control
           as="select"
           onChange={this.handleGradeChange}
           id={value}
+          col={col}
           defaultValue="0"
         >
           <option value="4">A</option>
@@ -74,6 +88,7 @@ class GPACalculator extends React.Component {
           min="0"
           max="5"
           id={value}
+          col={col}
         ></Form.Control>
       </Form>
     );
@@ -83,42 +98,58 @@ class GPACalculator extends React.Component {
     for (var i = 0; i <= this.count; i++) {
       sems.push(
         <div className="innerdiv" key={i}>
-          <Button onClick={this.insertNewSubject}>Add Subject</Button>
+          <Button onClick={this.insertNewSubject} id={i}>
+            Add Subject
+          </Button>
           <br />
-          {this.state.Subjects.map((val, index = 0) =>
-            this.returnSubjectJSON(index)
+          {this.state.Semesters[i].map((val, index = 0) =>
+            this.returnSubjectJSON(index, i)
           )}
-          <Button disabled>{this.calculateGPA()}</Button>
+          <Button disabled>{this.calculateGPA(i)}</Button>
         </div>
       );
     }
     return sems;
   }
-  addSemester() {
+  addSemester(event) {
+    let Sems = this.state.Semesters;
+    Sems.push([]);
     this.count++;
     this.forceUpdate();
     return this.getSems();
   }
-  calculateGPA() {
+  calculateCGPA() {
+    let CGPA = 0;
+    for (let i = 0; i < this.state.Semesters.length; i++) {
+      CGPA += parseFloat(this.calculateGPA(i));
+    }
+    return CGPA / this.state.Semesters.length;
+  }
+  calculateGPA(col) {
     let gradePoint = 0;
     let creditHourCount = 0;
-    for (var i = 0; i < this.state.Subjects.length; i++) {
+    for (var i = 0; i < this.state.Semesters[col].length; i++) {
       gradePoint +=
-        parseInt(this.state.Subjects[i].CreditHours) *
-        parseFloat(this.state.Subjects[i].Grade);
-      creditHourCount += parseInt(this.state.Subjects[i].CreditHours);
+        parseInt(this.state.Semesters[col][i].CreditHours) *
+        parseFloat(this.state.Semesters[col][i].Grade);
+      creditHourCount += parseInt(this.state.Semesters[col][i].CreditHours);
     }
     return isNaN(gradePoint / creditHourCount)
       ? 0
       : (gradePoint / creditHourCount).toFixed(2);
   }
-  insertNewSubject(prop) {
+  insertNewSubject(event) {
+    let id = parseInt(event.target.id);
+    console.log(id);
+    let Sems = this.state.Semesters;
+    console.log(Sems);
+    Sems[id].push({ Name: "", Grade: 0, CreditHours: 0 });
+    console.log(Sems);
     this.setState({
-      Subjects: [...this.state.Subjects, { Name: "", Grade: 0, CreditHours: 0 }]
+      Semesters: Sems
     });
   }
   render() {
-    console.log(this.count);
     return (
       <div id="maindiv">
         <Button id="NewSemester" onClick={this.addSemester}>
@@ -126,7 +157,7 @@ class GPACalculator extends React.Component {
         </Button>
         <div className="semesters">{this.getSems()}</div>
         <Button id="GPA" disabled>
-          {this.calculateGPA()}
+          {this.calculateCGPA()}
         </Button>
       </div>
     );
