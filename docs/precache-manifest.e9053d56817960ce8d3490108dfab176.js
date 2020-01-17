5bba1d5bfffd0ffba2f5b086794159cf5aa54632
MZO9400 <hamza.hameed00@yahoo.com>
2020-01-17 19:25:06 +0500

    [TEST] GitHub Pages test

---
+++ docs/precache-manifest.e9053d56817960ce8d3490108dfab176.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251362feecdb4e2dbf0123b76b4b276f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fe6238ea84c9ccb7340",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d66d030766bcd1a862ee",
+    "url": "/static/css/main.ef046f61.chunk.css"
+  },
+  {
+    "revision": "3fe6238ea84c9ccb7340",
+    "url": "/static/js/2.877b48c6.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.877b48c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d66d030766bcd1a862ee",
+    "url": "/static/js/main.d59bc404.chunk.js"
+  },
+  {
+    "revision": "03357149483a376b5b2e",
+    "url": "/static/js/runtime-main.b09effc8.js"
+  }
+]);
